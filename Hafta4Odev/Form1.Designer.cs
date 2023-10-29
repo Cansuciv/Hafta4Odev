532908ae3f507f34c1826f5f82820426866598c7
CANSU ÇİV <cansuciv@posta.mu.edu.tr>
2023-10-29 12:30:54 +0300

    Add project files.

---
+++ Hafta4Odev/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Hafta4Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            button1 = new Button();
+            Durum = new TextBox();
+            RafladigiYer = new TextBox();
+            Dil = new TextBox();
+            SayfaSayisi = new TextBox();
+            KitapTuru = new TextBox();
+            YayinTarihi = new TextBox();
+            Yayinevi = new TextBox();
+            KitapYazari = new TextBox();
+            KitapAdi = new TextBox();
+            label7 = new Label();
+            label9 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.DarkSalmon;
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(Durum);
+            groupBox1.Controls.Add(RafladigiYer);
+            groupBox1.Controls.Add(Dil);
+            groupBox1.Controls.Add(SayfaSayisi);
+            groupBox1.Controls.Add(KitapTuru);
+            groupBox1.Controls.Add(YayinTarihi);
+            groupBox1.Controls.Add(Yayinevi);
+            groupBox1.Controls.Add(KitapYazari);
+            groupBox1.Controls.Add(KitapAdi);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.Location = new Point(44, 13);
+            groupBox1.Margin = new Padding(4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4);
+            groupBox1.Size = new Size(608, 677);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Kitap Ekle";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(8, 481);
+            label8.Name = "label8";
+            label8.Size = new Size(156, 28);
+            label8.TabIndex = 20;
+            label8.Text = "Raflanadığı yer : ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(438, 606);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 50);
+            button1.TabIndex = 19;
+            button1.Text = "Ekle";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Durum
+            // 
+            Durum.Location = new Point(173, 540);
+            Durum.Name = "Durum";
+            Durum.Size = new Size(365, 34);
+            Durum.TabIndex = 18;
+            // 
+            // RafladigiYer
+            // 
+            RafladigiYer.Location = new Point(173, 480);
+            RafladigiYer.Name = "RafladigiYer";
+            RafladigiYer.Size = new Size(365, 34);
+            RafladigiYer.TabIndex = 17;
+            // 
+            // Dil
+            // 
+            Dil.Location = new Point(173, 420);
+            Dil.Name = "Dil";
+            Dil.Size = new Size(365, 34);
+            Dil.TabIndex = 16;
+            // 
+            // SayfaSayisi
+            // 
+            SayfaSayisi.Location = new Point(173, 360);
+            SayfaSayisi.Name = "SayfaSayisi";
+            SayfaSayisi.Size = new Size(365, 34);
+            SayfaSayisi.TabIndex = 15;
+            // 
+            // KitapTuru
+            // 
+            KitapTuru.Location = new Point(173, 300);
+            KitapTuru.Name = "KitapTuru";
+            KitapTuru.Size = new Size(365, 34);
+            KitapTuru.TabIndex = 14;
+            // 
+            // YayinTarihi
+            // 
+            YayinTarihi.Location = new Point(173, 240);
+            YayinTarihi.Name = "YayinTarihi";
+            YayinTarihi.Size = new Size(365, 34);
+            YayinTarihi.TabIndex = 13;
+            // 
+            // Yayinevi
+            // 
+            Yayinevi.Location = new Point(173, 180);
+            Yayinevi.Name = "Yayinevi";
+            Yayinevi.Size = new Size(365, 34);
+            Yayinevi.TabIndex = 12;
+            // 
+            // KitapYazari
+            // 
+            KitapYazari.Location = new Point(173, 120);
+            KitapYazari.Name = "KitapYazari";
+            KitapYazari.Size = new Size(365, 34);
+            KitapYazari.TabIndex = 11;
+            // 
+            // KitapAdi
+            // 
+            KitapAdi.Location = new Point(173, 60);
+            KitapAdi.Name = "KitapAdi";
+            KitapAdi.Size = new Size(365, 34);
+            KitapAdi.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(115, 421);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 28);
+            label7.TabIndex = 9;
+            label7.Text = "Dil : ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(69, 541);
+            label9.Name = "label9";
+            label9.Size = new Size(86, 28);
+            label9.TabIndex = 8;
+            label9.Text = "Durum : ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(49, 301);
+            label6.Name = "label6";
+            label6.Size = new Size(116, 28);
+            label6.TabIndex = 5;
+            label6.Text = "Kitap Türü : ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(38, 361);
+            label5.Name = "label5";
+            label5.Size = new Size(127, 28);
+            label5.TabIndex = 4;
+            label5.Text = "Sayfa Sayısı : ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(43, 241);
+            label4.Name = "label4";
+            label4.Size = new Size(122, 28);
+            label4.TabIndex = 3;
+            label4.Text = "Yayın Tarihi : ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(69, 181);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Yayınevi : ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 121);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Kitap Yazarı : ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(61, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Kitap Adı : ";
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.DarkSalmon;
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(listBox1);
+            groupBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox2.Location = new Point(748, 13);
+            groupBox2.Margin = new Padding(4);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(4);
+            groupBox2.Size = new Size(560, 600);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Kitap Sorgula";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(43, 424);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(485, 36);
+            comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(381, 506);
+            button2.Name = "button2";
+            button2.Size = new Size(147, 47);
+            button2.TabIndex = 2;
+            button2.Text = "Temizle";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 28;
+            listBox1.Location = new Point(43, 58);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(475, 312);
+            listBox1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(989, 656);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 51);
+            button3.TabIndex = 7;
+            button3.Text = "KAPAT";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1371, 719);
+            Controls.Add(button3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label8;
+        private Button button1;
+        private TextBox Durum;
+        private TextBox RafladigiYer;
+        private TextBox Dil;
+        private TextBox SayfaSayisi;
+        private TextBox KitapTuru;
+        private TextBox YayinTarihi;
+        private TextBox Yayinevi;
+        private TextBox KitapYazari;
+        private TextBox KitapAdi;
+        private Label label7;
+        private Label label9;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private ComboBox comboBox1;
+        private Button button2;
+        private ListBox listBox1;
+        private Button button3;
+    }
+}
